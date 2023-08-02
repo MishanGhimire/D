145719dddcfb80e731acd5edfe1447078e81811d
mgrsuyog <mgrsuyog6@gmail.com>
2023-08-02 21:51:34 +0545

    Calculator

--- eclipse/Calculator/src/com/calc/Calcv2.java
+++ eclipse/Calculator/src/com/calc/Calcv2.java
@@ -1,7 +1,11 @@
 package com.calc;
 
-public class Calcv2 {
+public class Calcv2 extends Calc {
 	public int div(int a, int b) {
 		return a/b;
+	}
+	
+	public int multi(int a, int b) {
+		return a*b;
 	}
 }
--- eclipse/Calculator/src/com/calc/Main.java
+++ eclipse/Calculator/src/com/calc/Main.java
@@ -2,11 +2,12 @@ package com.calc;
 
 public class Main {
 	public static void main(String[] args) {
-		Calc calc = new Calc();
+		Calcv2 calc = new Calcv2();
 		
 		int s = calc.add(8, 9);
 		int s1 = calc.sub(10, 5);
-		
-		System.out.println(s+" : "+s1);
+		int s2= calc.div(8, 8);
+		int s3= calc.multi(10, 5);
+		System.out.println(s+" : "+s1+ ": "+ s2 + " : "+ s3);
 	}
 }
