977976ddaae8dcc159f0a85800db31bcbdc6e8d0
mgrsuyog <mgrsuyog6@gmail.com>
2023-08-03 20:11:57 +0545

    nothing new

--- MainCalc.java
+++ MainCalc.java
@@ -1,11 +1,12 @@
-import calculator.Calc;
+import calculator.Calcv1;
 
 public class MainCalc {
     public static void main(String[] args) {
-        Calc calc = new Calc();
+        Calcv1 calc = new Calcv1();
         int r1 = calc.add(8, 10);
         int r2 = calc.sub(10, 8);
+        int r3 = calc.multi(8,9);
 
-        System.out.println(r1 + " " + r2);
+        System.out.println(r1 + " " + r2 + " "+ r3);
     }
 }
---
+++ calculator/Calcv1.java
@@ -0,0 +1,7 @@
+package calculator;
+
+public class Calcv1 extends Calc{
+    public int multi(int x , int y){
+        return x*y;
+    }
+}
