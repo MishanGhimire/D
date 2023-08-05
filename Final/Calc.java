43e5352bf8982c9c0ad4c03a70dc87b98358938b
mgrsuyog <mgrsuyog6@gmail.com>
2023-08-05 16:29:22 +0545

    done

---
+++ Final/Calc.java
@@ -0,0 +1,11 @@
+package Final;
+
+final public class Calc {
+     public void show(){
+        System.out.println("In obj show");
+     }
+
+     public void add (int a, int b){
+        System.out.println(a+b);
+     }
+}
